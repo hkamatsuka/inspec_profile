df5894b444e1ce584319f7b72abf760964be6d6c
hkamatsuka <hoge@example.com>
2018-03-17 09:48:50 +0900

    fix

--- controls/example.rb
+++ controls/example.rb
@@ -2,6 +2,7 @@
 # copyright: 2015, The Authors
 # license: All rights reserved
 
+test_dir = '/default'
 title 'sample section'
 
 # you can also use plain tests
@@ -14,7 +15,7 @@ control 'tmp-1.0' do                        # A unique ID for this control
   impact 0.7                                # The criticality, if this control fails.
   title 'Create /tmp directory'             # A human-readable title
   desc 'An optional description...'
-  describe file('/tmp') do                  # The actual test
+  describe file(test_dir) do                  # The actual test
     it { should be_directory }
   end
 end
